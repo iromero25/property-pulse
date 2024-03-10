253895740eed63571459ad9fa9349bc9bee30dab
iromero25 <romero81@yahoo.com>
2024-03-10 23:37:51 +0000

    add loading ans not-found pages

---
+++ app/loading.tsx
@@ -0,0 +1,22 @@
+import ClipLoader from "react-spinners/ClipLoader";
+
+const override = {
+  display: "block",
+  margin: "100px auto",
+};
+
+interface LoadingPageProps {
+  loading: boolean;
+}
+
+export default function LoadingPage({ loading }: LoadingPageProps) {
+  return (
+    <ClipLoader
+      color="#3b82f6"
+      loading={loading}
+      cssOverride={override}
+      size={150}
+      aria-label="Loading Spinner"
+    />
+  );
+}
