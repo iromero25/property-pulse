52ed35f4dacaaf8f392934525893fad3fd1c27b3
iromero25 <romero81@yahoo.com>
2024-03-10 23:40:46 +0000

    add the database config and api property routes

---
+++ utils/requests.ts
@@ -0,0 +1,47 @@
+import { PropertySchemaType } from "@/models/Property";
+
+const apiDomain = process.env.NEXT_PUBLIC_API_DOMAIN || null;
+
+// Fetch all properties
+export async function fetchProperties(): Promise<Array<PropertySchemaType>> {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return [];
+    }
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_DOMAIN}/properties`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return [];
+  }
+}
+
+// Fetch single property
+export async function fetchProperty(
+  id: string
+): Promise<PropertySchemaType | null> {
+  try {
+    // Handle the case where the domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
